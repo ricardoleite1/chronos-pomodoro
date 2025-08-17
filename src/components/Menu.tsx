b9fb0f705f37427b2b04bdd4892a39606326ddaa
Ricardo <ricardoleitejr99@gmail.com>
2025-08-17 13:39:40 -0300

    adding anchors on menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,23 +2,31 @@ import { History, Settings, Sun, House } from "lucide-react";
 
 export function Menu() {
   return (
-    <div className='flex items-center gap-6 mt-12'>
-      <House
-        size={54}
-        className='text-zinc-800 cursor-pointer hover:text-zinc-900 transition ease-in-out delay-50 bg-cyan-500 p-3 rounded-2xl'
-      />
-      <History
-        size={54}
-        className='text-zinc-800 cursor-pointer hover:text-zinc-900 transition ease-in-out delay-50 bg-cyan-500 p-3 rounded-2xl'
-      />
-      <Settings
-        size={54}
-        className='text-zinc-800 cursor-pointer hover:text-zinc-900 transition ease-in-out delay-50 bg-cyan-500 p-3 rounded-2xl'
-      />
-      <Sun
-        size={54}
-        className='text-zinc-800 cursor-pointer hover:text-zinc-900 transition ease-in-out delay-50 bg-cyan-500 p-3 rounded-2xl'
-      />
-    </div>
+    <nav className='flex items-center gap-6 mt-12'>
+      <a href='#'>
+        <House
+          size={54}
+          className='text-zinc-700 cursor-pointer hover:text-zinc-900 transition ease-in-out delay-50 bg-cyan-500 p-3 rounded-2xl'
+        />
+      </a>
+      <a href='#'>
+        <History
+          size={54}
+          className='text-zinc-700 cursor-pointer hover:text-zinc-900 transition ease-in-out delay-50 bg-cyan-500 p-3 rounded-2xl'
+        />
+      </a>
+      <a href='#'>
+        <Settings
+          size={54}
+          className='text-zinc-700 cursor-pointer hover:text-zinc-900 transition ease-in-out delay-50 bg-cyan-500 p-3 rounded-2xl'
+        />
+      </a>
+      <a href='#'>
+        <Sun
+          size={54}
+          className='text-zinc-700 cursor-pointer hover:text-zinc-900 transition ease-in-out delay-50 bg-cyan-500 p-3 rounded-2xl'
+        />
+      </a>
+    </nav>
   );
 }
