4cfab1d66b1c71273e3976294a619f65a8095ed0
Ricardo <ricardoleitejr99@gmail.com>
2025-08-21 21:30:39 -0300

    feat: formatSecondsToMinutes and getNextCycle functions

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,10 +2,62 @@ import { PlayCircleIcon } from "lucide-react";
 import { Button } from "./Button";
 import { Cycles } from "./Cycles";
 import { Input } from "./Input";
+import { useRef } from "react";
+import type { TaskModel } from "../models/TaskModel";
+import { useTaskContext } from "../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../utils/getNextCycle";
+import { getNextCycleType } from "../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../utils/formatSecondsToMinutes";
 
 export function Form() {
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
+
+  function handleStartNewTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("teste");
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+    if (!taskName) {
+      alert("Digite o nome da tarefa");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completedDate: null,
+      interruptDate: null,
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: {
+          ...prevState.config,
+        },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
   return (
     <form
+      onSubmit={handleStartNewTask}
       action=''
       className='flex flex-col justify-center items-center gap-10 text-center'
     >
@@ -14,6 +66,7 @@ export function Form() {
           label='Task'
           id='task'
           type='text'
+          ref={taskNameInput}
           placeholder='Digite a sua task'
         />
       </div>
