9a925eb2ac3a9535e34eeb2f59a356d34707db7f
Ricardo <ricardoleitejr99@gmail.com>
2025-08-24 12:35:59 -0300

    feat: router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import { MessagesContainer } from "./components/MessagesContainer";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
-import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
-import { AboutPomodoro } from "./pages/AboutPomodoro";
+import { Router } from "./routers/Router";
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about-pomodoro' element={<AboutPomodoro />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <Router />
       </MessagesContainer>
     </TaskContextProvider>
   );
---
+++ src/routers/Router.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes, useLocation } from "react-router";
+import { Home } from "../pages/Home";
+import { NotFound } from "../pages/NotFound";
+import { AboutPomodoro } from "../pages/AboutPomodoro";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return null;
+}
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about-pomodoro' element={<AboutPomodoro />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <ScrollToTop />
+    </BrowserRouter>
+  );
+}
